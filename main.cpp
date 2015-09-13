01f42eefaecc26432ecfa5b66e2681e3be840664
pejuan <jose.pejuan@unitec.edu>
2015-09-12 21:19:06 -0600

    Agregados comandos ls, cd, clear y exit

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <stdio.h>
+#include <functional>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <cstdlib>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+using namespace std;
+
+
+int getdir (string dir, vector<string> &files){
+	DIR *dp;
+	struct dirent *dirp;
+	if((dp = opendir(dir.c_str())) == NULL){
+		cout << "Error(" << errno << ") opening " << dir << endl;
+		return errno;
+	}
+	while ((dirp = readdir(dp)) != NULL){
+		files.push_back(string(dirp->d_name));
+	}
+	closedir(dp);
+	return 0;
+}
+int main(int argc, char const *argv[]){
+	
+	vector<string> listaComandos;
+	bool verificacion = true;
+	string currentDirectory = "/home/pejuan/";
+	listaComandos.push_back("mkdir");
+	listaComandos.push_back("cd");
+	listaComandos.push_back("ls");
+	listaComandos.push_back("clear");
+	listaComandos.push_back("exit");
+	string ingreso;
+	while(true){
+		if (verificacion){
+			cout<<"mi_sh@"<<currentDirectory<<">";
+		}else{
+			
+		}
+		
+		getline(cin,ingreso);
+		if (find(listaComandos.begin(),listaComandos.end(),ingreso) != listaComandos.end()){
+			/* code */
+			if (ingreso=="mkdir"){
+
+			}else if(ingreso=="cd"){
+				string nuevo;
+				cin>>nuevo;
+				currentDirectory += (nuevo+"/");
+				verificacion = false;
+
+			}else if(ingreso=="clear"){
+				for (int i = 0; i < 25; i++){
+					cout<<endl;
+				}
+			}else if(ingreso=="ls"){
+				string dir = currentDirectory;
+				vector<string> files = vector<string>();
+				getdir(dir,files);
+				for (unsigned int i = 0;i < files.size();i++)
+				{
+				cout << files[i] << endl;
+				}
+			}else if(ingreso=="exit"){
+				break;
+			}
+		}else{
+			if (verificacion)
+			{
+				/* code */
+				cout<<"Comando no existe."<<endl;
+
+			}else{
+				verificacion = true;
+			}
+			
+		}
+
+	}
+
+
+	return 0;
+}	
