6c7a8d3b6a75a37fb863256e6f15efb58ee815bb
Jorge Caballero <jorge@2build.it>
2015-09-21 16:22:27 -0600

    reordenando las funciones

--- shell.cpp
+++ shell.cpp
@@ -22,201 +22,16 @@
 #include <ctype.h>
 
 using namespace std;
+bool contains(char* comando, vector<string> listaComandos);
+bool containsStr(string comando, vector<string> listaComandos);
+int input_pipe_output(char *inred,char *p,char *s[],char *outred,int n);
+int single_output_redirect(char *s1,char *s2);
+int out_pipe(char *s[],char *outred,int n);
+int mul_pipe(char *s[],int n);
+int input_pipe(char *inred,char *p,char *s[],int n);
+int input_output(char *s1,char *s2,char *s3);
+int  single_input_redirect(char *s1,char *s2);
 
-bool contains(char* comando, vector<string> listaComandos){
-	for (int i = 0; i < listaComandos.size(); ++i){
-		if (!strcmp(comando,listaComandos[i].c_str())){
-			return true;
-		}
-
-	}
-	return false;
-}
-
-bool containsStr(string comando, vector<string> listaComandos){
-	for (int i = 0; i < listaComandos.size(); ++i){
-		if (comando==listaComandos[i]){
-			return true;
-		}
-
-	}
-	return false;
-}
-
-int salida_a_pipe(char *s[],char *outred,int n)
-{
-    int pip[2*n];
-    for(int i=0;i<2*n;i=i+2)
-    {
-        pipe(pip+i);
-    }
-    int status;
-    int out;
-    char *dest=strtok(outred," ");
-    out = open(dest, O_WRONLY | O_TRUNC | O_CREAT|O_APPEND,0700);
-    for(int j=0;j<n+1;j++)
-    {
-        char *t;
-        char *argv[1024];
-        t=strtok(s[j]," ");
-        argv[0]=t;
-        int m=1;
-        while(t!=NULL)
-        {
-        t=strtok(NULL," ");
-        if(t==NULL)
-            break;
-        argv[m]=t;
-        m++;
-        }
-        argv[m]=NULL;
-        if(fork()==0)
-        {
-            if(j==0)
-            {
-                dup2(pip[1],1);
-                for(int k=0;k<2*n;k++)
-                    close(pip[k]);
-                 execvp(*argv, argv);
-            }
-           else if(j==n)
-            {
-                dup2(pip[2*n-2],0);
-                dup2(out,1);
-
-                    for(int k=0;k<2*n;k++)
-                    close(pip[k]);
-                  close(out);
-                 execvp(*argv, argv);
-            }
-            else
-            {
-                dup2(pip[2*j+1],1);
-                dup2(pip[2*(j-1)],0);
-                for(int k=0;k<2*n;k++)
-                    close(pip[k]);
-                 execvp(*argv, argv);
-            }
-        }
-    }
-     for(int k=0;k<2*n;k++)
-        close(pip[k]);
-     close(out);
-    for(int k=0;k<2*n-1;k++)
-        wait(&status);
-}
-
-int redireccion_de_salida_unica(char *s1,char *s2)
-{
-    char *t=strtok(s1," ");
-    char *argv[100];
-    argv[0]=t;
-    int i=1,status;
-    while(t!=NULL)
-    {
-        t=strtok(NULL," ");
-        if(t==NULL)
-            break;
-        argv[i]=t;
-        i++;
-    }
-    argv[i]=NULL;
-    char *dest=s2;
-    int out;
-    out = open(dest, O_WRONLY | O_TRUNC | O_CREAT|O_APPEND,0700);
-    if (fork() == 0)
-    {
-        dup2(out,1);
-        close(out);
-        execvp(*argv,argv);
-    }
-
-     close(out);
-  for (int i = 0; i < 1; i++)
-    wait(&status);
-
-}
- int entrada_pipe_salida(char *inred,char *p,char *s[],char *outred,int n)
- {
-    int in,status;
-    char *inargv[100];
-    char *t3=strtok(inred," ");
-    inargv[0]=t3;
-    int k=1;
-    while(t3!=NULL)
-    {
-        t3=strtok(NULL," ");
-        if(t3==NULL)
-            break;
-        inargv[k]=t3;
-        k++;
-    }
-    inargv[k]=NULL;
-    char *path=strtok(p," ");
-    in=open(path,O_RDONLY);
-    int pip[2*n];
-    for(int i=0;i<2*n;i=i+2)
-    {
-        pipe(pip+i);
-    }
-    int out;
-    char *dest=strtok(outred," ");
-    out = open(dest, O_WRONLY | O_TRUNC | O_CREAT|O_APPEND,0700);
-    for(int j=0;j<n+1;j++)
-    {
-        char *t;
-        char *argv[1024];
-        t=strtok(s[j]," ");
-        argv[0]=t;
-        int m=1;
-        while(t!=NULL)
-        {
-        t=strtok(NULL," ");
-        if(t==NULL)
-            break;
-        argv[m]=t;
-        m++;
-        }
-        argv[m]=NULL;
-        if(fork()==0)
-        {
-            if(j==0)
-            {
-               dup2(in,0);
-               dup2(pip[1],1);
-                for(int k=0;k<2*n;k++)
-                    close(pip[k]);
-                close(in);
-                 execvp(*inargv,inargv);
-            }
-           else if(j==n)
-            {
-                dup2(pip[2*n-2],0);
-                dup2(out,1);
-
-                    for(int k=0;k<2*n;k++)
-                    close(pip[k]);
-                  close(out);
-                 execvp(*argv, argv);
-            }
-            else
-            {
-                dup2(pip[2*j+1],1);
-                dup2(pip[2*(j-1)],0);
-                for(int k=0;k<2*n;k++)
-                    close(pip[k]);
-                 execvp(*argv, argv);
-            }
-        }
-    }
-     for(int k=0;k<2*n;k++)
-        close(pip[k]);
-    close(in);
-    close(out);
-    for(int k=0;k<2*n-1;k++)
-        wait(&status);
-
-}
 
 char CDIR[1024]; 
 
@@ -431,4 +246,410 @@ int main(int argc, char const *argv[])
 	} // fin while true
 	
 	return 0;
+}
+
+//function for multiple pipe implementation
+int mul_pipe(char *s[],int n)
+{
+    int pip[2*n];
+    for(int i=0;i<2*n;i=i+2)
+    {
+        pipe(pip+i);
+    }
+    int status;
+    for(int j=0;j<n+1;j++)
+    {
+        char *t;
+        char *argv[1024];
+        t=strtok(s[j]," ");
+        argv[0]=t;
+        int m=1;
+        while(t!=NULL)
+        {
+            t=strtok(NULL," ");
+            if(t==NULL)
+                break;
+            argv[m]=t;
+            m++;
+        }
+        argv[m]=NULL;
+        if(fork()==0)
+        {
+            if(j==0)
+            {
+                dup2(pip[1],1);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+           else if(j==n)
+            {
+                dup2(pip[2*n-2],0);
+                    for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+            else
+            {
+                dup2(pip[2*j+1],1);
+                dup2(pip[2*(j-1)],0);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+        }
+    }
+    for(int k=0;k<2*n;k++)
+        close(pip[k]);
+    for(int k=0;k<2*n-1;k++)
+        wait(&status);
+
+}
+//function for input redirection with single pipe
+int input_pipe(char *inred,char *p,char *s[],int n)
+{
+    int in,status;
+    char *inargv[100];
+    char *t3=strtok(inred," ");
+    inargv[0]=t3;
+    int k=1;
+    while(t3!=NULL)
+    {
+        t3=strtok(NULL," ");
+        if(t3==NULL)
+            break;
+        inargv[k]=t3;
+        k++;
+    }
+    inargv[k]=NULL;
+    char *path=strtok(p," ");
+    in=open(path,O_RDONLY);
+    int pip[2*n];
+    for(int i=0;i<2*n;i=i+2)
+    {
+        pipe(pip+i);
+    }
+
+    for(int j=0;j<n+1;j++)
+    {
+        char *t;
+        char *argv[1024];
+        t=strtok(s[j]," ");
+        argv[0]=t;
+        int m=1;
+        while(t!=NULL)
+        {
+            t=strtok(NULL," ");
+            if(t==NULL)
+                break;
+            argv[m]=t;
+            m++;
+        }
+        argv[m]=NULL;
+        if(fork()==0)
+        {
+            if(j==0)
+            {
+               dup2(in,0);
+               dup2(pip[1],1);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                close(in);
+                execvp(*inargv,inargv);
+            }
+           else if(j==n)
+            {
+                dup2(pip[2*n-2],0);
+                    for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+            else
+            {
+                dup2(pip[2*j+1],1);
+                dup2(pip[2*(j-1)],0);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+        }
+    }
+    for(int k=0;k<2*n;k++)
+        close(pip[k]);
+    close(in);
+    for(int k=0;k<2*n-1;k++)
+        wait(&status);
+}
+//function for input output redirection
+int input_output(char *s1,char *s2,char *s3)
+{
+   char *t1,t2;
+   int status;
+   t1=strtok(s1," ");
+   char *argv[100];
+   argv[0]=t1;
+   int i=1;
+   while(t1!=NULL)
+   {
+        t1=strtok(NULL," ");
+        if(t1==NULL)
+            break;
+        argv[i]=t1;
+        i++;
+   }
+   argv[i]=NULL;
+   char *path=strtok(s2," ");
+   char *dest=strtok(s3," ");
+   int in,out;
+   in = open(path, O_RDONLY);
+   out = open(dest, O_WRONLY | O_TRUNC | O_CREAT|O_APPEND,0700);
+    if (fork() == 0)
+    {
+        dup2(in, 0);
+        dup2(out,1);
+        close(in);
+        close(out);
+        execvp(*argv,argv);
+    }
+    close(in);
+    close(out);
+    for (int i = 0; i < 2; i++)
+      wait(&status);
+
+}
+//function for single input redirect
+int  single_input_redirect(char *s1,char *s2)
+{
+    int in,status;
+    char *argv[100];
+    char *t;
+   // t=strtok(s," ");
+   // argv5[0]=t5;
+    t=strtok(s1," ");
+    argv[0]=t;
+    int k=1;
+    while(t!=NULL)
+    {
+        t=strtok(NULL," ");
+        if(t==NULL)
+            break;
+        argv[k]=t;
+        k++;
+    }
+    argv[k]=NULL;
+   // t=strtok(NULL,"<");
+    char *path=strtok(s2," ");
+    in=open(path,O_RDONLY);
+    if (fork() == 0)
+    {
+
+        dup2(in, 0);
+        close(in);
+        execvp(*argv,argv);
+    }
+
+     close(in);
+
+
+
+  for (int i = 0; i < 1; i++)
+    wait(&status);
+
+
+}
+//function with output redirect with pipe
+int out_pipe(char *s[],char *outred,int n)
+{
+    //cout<<"out red value in function"<<outred;
+    int pip[2*n];
+    for(int i=0;i<2*n;i=i+2)
+    {
+        pipe(pip+i);
+    }
+    int status;
+    int out;
+    char *dest=strtok(outred," ");
+    out = open(dest, O_WRONLY | O_TRUNC | O_CREAT|O_APPEND,0700);
+    for(int j=0;j<n+1;j++)
+    {
+        char *t;
+        char *argv[1024];
+        t=strtok(s[j]," ");
+        argv[0]=t;
+        int m=1;
+        while(t!=NULL)
+        {
+        t=strtok(NULL," ");
+        if(t==NULL)
+            break;
+        argv[m]=t;
+        m++;
+        }
+        argv[m]=NULL;
+        if(fork()==0)
+        {
+            if(j==0)
+            {
+                dup2(pip[1],1);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+           else if(j==n)
+            {
+                dup2(pip[2*n-2],0);
+                dup2(out,1);
+
+                    for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                  close(out);
+                 execvp(*argv, argv);
+            }
+            else
+            {
+                dup2(pip[2*j+1],1);
+                dup2(pip[2*(j-1)],0);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+        }
+    }
+     for(int k=0;k<2*n;k++)
+        close(pip[k]);
+     close(out);
+    for(int k=0;k<2*n-1;k++)
+        wait(&status);
+
+}
+int single_output_redirect(char *s1,char *s2)
+{
+    char *t=strtok(s1," ");
+    char *argv[100];
+    argv[0]=t;
+    int i=1,status;
+    while(t!=NULL)
+    {
+        t=strtok(NULL," ");
+        if(t==NULL)
+            break;
+        argv[i]=t;
+        i++;
+    }
+    argv[i]=NULL;
+    char *dest=s2;
+    int out;
+    out = open(dest, O_WRONLY | O_TRUNC | O_CREAT|O_APPEND,0700);
+    if (fork() == 0)
+    {
+        dup2(out,1);
+        close(out);
+        execvp(*argv,argv);
+    }
+
+     close(out);
+  for (int i = 0; i < 1; i++)
+    wait(&status);
+
+}
+ int input_pipe_output(char *inred,char *p,char *s[],char *outred,int n)
+ {
+    int in,status;
+    char *inargv[100];
+    char *t3=strtok(inred," ");
+    inargv[0]=t3;
+    int k=1;
+    while(t3!=NULL)
+    {
+        t3=strtok(NULL," ");
+        if(t3==NULL)
+            break;
+        inargv[k]=t3;
+        k++;
+    }
+    inargv[k]=NULL;
+    char *path=strtok(p," ");
+    in=open(path,O_RDONLY);
+    int pip[2*n];
+    for(int i=0;i<2*n;i=i+2)
+    {
+        pipe(pip+i);
+    }
+    int out;
+    char *dest=strtok(outred," ");
+    out = open(dest, O_WRONLY | O_TRUNC | O_CREAT|O_APPEND,0700);
+    for(int j=0;j<n+1;j++)
+    {
+        char *t;
+        char *argv[1024];
+        t=strtok(s[j]," ");
+        argv[0]=t;
+        int m=1;
+        while(t!=NULL)
+        {
+        t=strtok(NULL," ");
+        if(t==NULL)
+            break;
+        argv[m]=t;
+        m++;
+        }
+        argv[m]=NULL;
+        if(fork()==0)
+        {
+            if(j==0)
+            {
+               dup2(in,0);
+               dup2(pip[1],1);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                close(in);
+                 execvp(*inargv,inargv);
+            }
+           else if(j==n)
+            {
+                dup2(pip[2*n-2],0);
+                dup2(out,1);
+
+                    for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                  close(out);
+                 execvp(*argv, argv);
+            }
+            else
+            {
+                dup2(pip[2*j+1],1);
+                dup2(pip[2*(j-1)],0);
+                for(int k=0;k<2*n;k++)
+                    close(pip[k]);
+                 execvp(*argv, argv);
+            }
+        }
+    }
+     for(int k=0;k<2*n;k++)
+        close(pip[k]);
+    close(in);
+    close(out);
+    for(int k=0;k<2*n-1;k++)
+        wait(&status);
+
+}
+
+bool contains(char* comando, vector<string> listaComandos){
+	for (int i = 0; i < listaComandos.size(); ++i){
+		if (!strcmp(comando,listaComandos[i].c_str())){
+			return true;
+		}
+
+	}
+	return false;
+}
+
+bool containsStr(string comando, vector<string> listaComandos){
+	for (int i = 0; i < listaComandos.size(); ++i){
+		if (comando==listaComandos[i]){
+			return true;
+		}
+
+	}
+	return false;
 }
